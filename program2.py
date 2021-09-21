1ecfd28637b3de83d1d9a6345e12ace0d42cb5f0
shagup1 <91088477+shagup1@users.noreply.github.com>
2021-09-21 03:39:45 -0700

    Create program2.py

---
+++ program2.py
@@ -0,0 +1,13 @@
+a=int(input("enter how much do you want to print"))
+for num in range(0,a+1):
+    if(num%2!=0):
+        print(num,end=",")
+    elif(a==x):
+        for num in list[1:]:
+            print(num,end=",")
+        print()
+    
+        
+            
+            
+
